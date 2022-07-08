b2c3b83b1421b4500d2000d0a4ebd97d561eb844
Liuxxx3 <631083327@qq.com>
2022-07-08 19:50:30 +0800

    0708

--- vit_mmco/train.py
+++ vit_mmco/train.py
@@ -11,23 +11,27 @@ from tensorboard_logger import Logger
 
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-def train(itr, dataset, args, model, optimizer, logger, device):
+def train(itr, dataset, args, model: torch.nn.DataParallel, optimizer, logger, device):
     model.train()
     features,audio_features, labels, pairs_id = dataset.load_data(n_similar=args.num_similar)
-    seq_len = np.sum(np.max(np.abs(features), axis=2) > 0, axis=1)
-    features = features[:,:np.max(seq_len),:]
-    audio_features = audio_features[:,:np.max(seq_len),:]
+    # seq_len = np.sum(np.max(np.abs(features), axis=2) > 0, axis=1)
+    # features = features[:,:np.max(seq_len),:]
+    # audio_features = audio_features[:,:np.max(seq_len),:]
     
-    features = torch.from_numpy(features).float().to(device)
-    audio_features = torch.from_numpy(audio_features).float().to(device)
+    # features = torch.from_numpy(features).float().to(device)
+    for i in range(0,features.shape[0]):
+        features[i] = torch.from_numpy(features[i]).float().to(device)
+    for i in range(0,features.shape[0]):
+        audio_features[i] = torch.from_numpy(audio_features[i]).float().to(device)
+    # audio_features = torch.from_numpy(audio_features).float().to(device)
     labels = torch.from_numpy(labels).float().to(device)
     #interative
     pseudo_label = None
    
-    outputs = model(features,audio_features,seq_len=seq_len,is_training=True,itr=itr,opt=args)
-    
+    outputs = model(features,audio_features,is_training=True,itr=itr,opt=args)
+    # import pdb; pdb.set_trace()
     total_loss,loss_mil_orig,loss_mil_supp,loss_3_supp_Contrastive,mutual_loss,loss_norm,loss_guide \
-        = model.criterion(outputs,labels,seq_len=seq_len,device=device,logger=logger,opt=args,itr=itr,pairs_id=pairs_id,inputs=features)
+        = model.module.criterion(outputs,labels,device=device,logger=logger,opt=args,itr=itr,pairs_id=pairs_id,inputs=features)
     # print('Iteration: %d, Loss: %.3f' %(itr, total_loss.data.cpu().numpy()))
     optimizer.zero_grad()
     torch.autograd.set_detect_anomaly(True)
